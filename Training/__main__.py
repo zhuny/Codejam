315d9de2b0c5e95d60ccd6b438a3370fe9efc018
Jihun Yang <zhuny@kaist.ac.kr>
2019-03-25 21:49:40 +0900

    TLE나는거 보안

--- Training/__main__.py
+++ Training/__main__.py
@@ -14,22 +14,27 @@ def get_ints():
 def do_one_step():
     N, P = get_ints()
     players = get_ints()
-    players.sort()
 
-    befores = []
-    before_total = 0
-    min_valid = []
-
+    # count sort를 구현해 뒀는데, 사실 builtins를 써도 상관없음
+    # TLE가 나는건 밑에서 before.pop(0)같은 코드를 짜둬서 그렇다.
+    count_list = [0 for i in range(10001)]
     for player in players:
-        if len(befores) == P:
-            before_total -= befores[0]
-            befores.pop(0)
-        befores.append(player)
-        before_total += player
-        if len(befores) == P:
-            min_valid.append(player*P-before_total)
+        count_list[player] += 1
+    current = 0
+    for index, count in enumerate(count_list):
+        for i in range(current, current+count):
+            players[i] = index
+        current += count
 
-    return min(min_valid)
+    total_sum = 0
+    min_value = []
+    for index, player in enumerate(players, 1):
+        total_sum += player
+        if index >= P:
+            min_value.append(player*P-total_sum)
+            total_sum -= players[index-P]
+
+    return min(min_value)
 
 
 def main():
